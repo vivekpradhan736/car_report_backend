3398f8b2323434a7048aa1b9bada8ecf0610e340
Vivek <vivekpradhan736@gmail.com>
2025-09-09 13:12:18 +0530

    new commit

--- server.js
+++ server.js
@@ -15,24 +15,24 @@ app.use(express.json());
 app.use(cookieParser());
 
 const allowedOrigins = ['https://car-report-frontend.vercel.app/'];
-// app.use(
-//   cors({
-//     origin: allowedOrigins, // only allow your frontend
-//     credentials: true,         // allow cookies to be sent
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
-app.use(cors({
-origin: (origin, callback) => {
-if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-callback(null, true);
-} else {
-callback(new Error('Not allowed by CORS'));
-}
-},
-credentials: true,
-}));
+app.use(
+  cors({
+    origin: allowedOrigins, // only allow your frontend
+    credentials: true,         // allow cookies to be sent
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+// app.use(cors({
+// origin: (origin, callback) => {
+// if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+// callback(null, true);
+// } else {
+// callback(new Error('Not allowed by CORS'));
+// }
+// },
+// credentials: true,
+// }));
 
 // Routes
 app.use("/api/auth", authRoutes);
